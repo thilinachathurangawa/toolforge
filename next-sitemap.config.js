940b4dfd357369c61bec85c7ca8aaa179bbd7c50
Thilina Wanniarachchi <thilinaw@jrcsoftware.com>
2026-06-07 23:52:48 +0530

    domain update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://toolforge-jet.vercel.app',
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://www.toolforge.website',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
